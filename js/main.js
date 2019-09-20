fe3d050f58f3a61b9944c1bdaf820008bc682777
jhoset10 <jhosetceron@aol.com>
2019-09-20 19:45:34 -0400

    new file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+// Script to open and close sidebar
+function side_open() {
+document.getElementById("mySidebar").style.display = "block";
+}
+
+function side_close() {
+document.getElementById("mySidebar").style.display = "none";
+}
