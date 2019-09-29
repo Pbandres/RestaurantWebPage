0372e934565cc16767385df4c5a0866beda756ed
jhoset10 <jhosetceron@aol.com>
2019-09-29 17:38:20 -0400

    Modified structure for html and new styles for css

--- js/main.js
+++
@@ -1,9 +0,0 @@
-
-// Script to open and close sidebar
-function side_open() {
-document.getElementById("mySidebar").style.display = "block";
-}
-
-function side_close() {
-document.getElementById("mySidebar").style.display = "none";
-}
